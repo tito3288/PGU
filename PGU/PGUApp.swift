e80c2cd25b0f2c37543dc2915ac54bb4db3d7a22
Bryan Arambula <arambula722@gmail.com>
2023-11-21 09:12:41 -0500

    Initial Commit

---
+++ PGU/PGUApp.swift
@@ -0,0 +1,17 @@
+//
+//  PGUApp.swift
+//  PGU
+//
+//  Created by Bryan Arambula on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PGUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
